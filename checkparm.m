f5631ddd11b2deeb8885c516aa890c141cf00ac8
cogdong <ydydavid8523@gmail.com>
2015-12-14 21:19:48 +0800

    v 1.1
    add the TE info

--- checkparm.m
+++ checkparm.m
@@ -1,3 +1,7 @@
+% V1.1
+% add TE info
+% -ydy 2015.12.14-
+
 function checkparm
 clc
 [filename2,pathname2] = uigetfile('*.dcm','Select One DCM');
@@ -8,6 +12,7 @@ if isfield(imginfo, 'NumberOfTemporalPositions')
     fprintf(1, 'number of TRs(nrofvols) = %s\n', num2str(imginfo.NumberOfTemporalPositions));
 end
 fprintf(1, 'TR(ms) = %s\n', num2str(imginfo.RepetitionTime));
+fprintf(1, 'TE(ms) = %s\n', num2str(imginfo.EchoTime));
 fprintf(1, 'X resolution(imgx) = %s\n', num2str(imginfo.Width));
 fprintf(1, 'Y resolution(imgy) = %s\n', num2str(imginfo.Height));
 fprintf(1, 'pdimx = %s\n', num2str(imginfo.ReconstructionDiameter/imginfo.Width));
